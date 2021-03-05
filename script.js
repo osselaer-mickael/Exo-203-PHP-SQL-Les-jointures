5becd12f4dfeadd85d31f2cf499885a35fcc2665
Nux007 <007.nux@gmail.com>
2021-03-05 13:29:35 +0100

    Exo 203.

---
+++ script.js
@@ -0,0 +1,31 @@
+let data = {
+    datasets: [{
+        data: [
+            11,
+            16,
+            7,
+            3,
+            14
+        ],
+        backgroundColor: [
+            "#FF6384",
+            "#4BC0C0",
+            "#FFCE56",
+            "#E7E9ED",
+            "#36A2EB"
+        ],
+        label: 'My dataset' // for legend
+    }],
+    labels: [
+        "Red",
+        "Green",
+        "Yellow",
+        "Grey",
+        "Blue"
+    ]
+};
+let ctx = $("#myChart");
+new Chart(ctx, {
+    data: data,
+    type: 'polarArea'
+});
